cfe71fd93fc9e8ae9705249760becfd1a772ab4a
Jeff Mullan <jeffmullan@Jeffs-MacBook-Pro-2.local>
2018-02-27 19:49:37 -0500

    no

---
+++ Assets/javascript/game.js
@@ -0,0 +1,25 @@
+// =================== 
+// Week-4 Crystal Game 
+// ===================
+//
+
+//Declare Vars
+
+var wins;
+var losses;
+var randomStart;
+
+// Fresh Start Vars
+
+wins = 0;
+losses = 0;
+randomStart = 0;
+restart = 0;
+
+// Starting a game new (no wins + losses yet)
+<button "onClick()">TEXT HERE</button>;
+
+/*{
+alert("Hey, Would You Like to Play the Crystal Game?");
+}*/
+
